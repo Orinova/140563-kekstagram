e348bb2bdea415fa9d3ba09af7261f7fc10fceff
Наташа <leninlives@yandex.ru>
2018-06-20 02:02:59 +0700

    Правки: имена переменных, фикс сброса фильтра, setScale одной функцией

--- js/pictures.js
+++ js/pictures.js
@@ -1,10 +1,7 @@
 'use strict';
 
-var KEYCODE_ESC = 27;
 var PHOTOS_COUNT = 25;
 var AVATAR_VARIANTS = 6;
-var picturesSection = document.querySelector('.pictures');
-var bigPicture = document.querySelector('.big-picture');
 var likes = {
   MIN: 15,
   MAX: 200
@@ -29,6 +26,36 @@ var DESCRIPTIONS = [
   'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
   'Вот это тачка!'
 ];
+
+var KEYCODE_ESC = 27;
+var picturesSection = document.querySelector('.pictures');
+var gallery = document.querySelector('.big-picture');
+var galleryCloseBtn = document.querySelector('.big-picture__cancel');
+
+var uploadSection = document.querySelector('.img-upload');
+var uploadFile = uploadSection.querySelector('#upload-file');
+var uploadOverlay = uploadSection.querySelector('.img-upload__overlay');
+var uploadCloseBtn = uploadSection.querySelector('.img-upload__cancel');
+var uploadImgPreview = uploadSection.querySelector('.img-upload__preview');
+
+var scale = {
+  RANGE: 25,
+  MIN: 25,
+  MAX: 100,
+  DEFAULT: 100
+};
+var DEFAULT_EFFECT_VALUE = 100;
+var uploadEffectScale = uploadSection.querySelector('.scale');
+var uploadEffectsList = uploadSection.querySelector('.effects__list');
+var uploadEffectValue = uploadSection.querySelector('.scale__value').value;
+var uploadEffectPin = uploadSection.querySelector('.scale__pin');
+var uploadEffectLine = uploadSection.querySelector('.scale__line');
+
+var resizeControls = uploadSection.querySelector('.resize');
+var resizePlus = resizeControls.querySelector('.resize__control--plus');
+var resizeMinus = resizeControls.querySelector('.resize__control--minus');
+var resizeValue = resizeControls.querySelector('.resize__control--value');
+
 
 // Генератор случайных чисел
 var getRandomNum = function (min, max) {
@@ -106,165 +133,134 @@ var fillPicturesList = function (photos) {
 
 // Покажите элемент .big-picture, удалив у него класс .hidden
 // и заполните его данными из созданного массива
-var showBigPicture = function (photo) {
-  var commentsList = bigPicture.querySelector('.social__comments'); // список комментариев (ul)
-  var socialComment = bigPicture.querySelector('.social__comment').cloneNode(true);
+var showgallery = function (photo) {
+  var commentsList = gallery.querySelector('.social__comments'); // список комментариев (ul)
+  var socialComment = gallery.querySelector('.social__comment').cloneNode(true);
   socialComment.classList.add('social__comment--text'); // получили шаблон для комментрия (li)
   commentsList.innerHTML = '';
 
-  var fragmentBigPicture = document.createDocumentFragment();
+  var fragment = document.createDocumentFragment();
   for (var i = 0; i < photo.comments.length; i++) {
     var comment = socialComment.cloneNode(true);
     comment.querySelector('.social__picture').src = 'img/avatar-' + getRandomNum(1, AVATAR_VARIANTS) + '.svg';
     comment.querySelector('.social__text').textContent = photo.comments[i];
-    fragmentBigPicture.appendChild(comment);
+    fragment.appendChild(comment);
   }
-  commentsList.appendChild(fragmentBigPicture);
+  commentsList.appendChild(fragment);
 
   // Подставляем инфу в большую картинку
-  bigPicture.querySelector('.big-picture__img img').src = photo.url;
-  bigPicture.querySelector('.social__caption').textContent = photo.description;
-  bigPicture.querySelector('.likes-count').textContent = photo.likes;
-  bigPicture.querySelector('.comments-count').textContent = photo.comments.length;
+  gallery.querySelector('.big-picture__img img').src = photo.url;
+  gallery.querySelector('.social__caption').textContent = photo.description;
+  gallery.querySelector('.likes-count').textContent = photo.likes;
+  gallery.querySelector('.comments-count').textContent = photo.comments.length;
 
   // Спрячьте блоки - это по заданию module3-task1, пятый пункт
-  bigPicture.querySelector('.social__comment-count').classList.add('visually-hidden');
-  bigPicture.querySelector('.social__loadmore').classList.add('visually-hidden');
+  gallery.querySelector('.social__comment-count').classList.add('visually-hidden');
+  gallery.querySelector('.social__loadmore').classList.add('visually-hidden');
 };
 
 var photos = createContent(PHOTOS_COUNT);
 fillPicturesList(photos);
 
 // ------ 1. Начало: Галерея
-var picturePreviewClickHandler = function (evt) {
+var onPhotoClick = function (evt) {
   var target = evt.target;
   if (target.className === 'picture__img') {
     target = target.parentNode;
-    var photoIndexNum = target.getAttribute('id').substr(6); // забираю число из id вида photo_00
-    showBigPicture(photos[photoIndexNum]);
-    openGalleryPopup();
+    var photoIndex = target.getAttribute('id').substr(6);
+    showgallery(photos[photoIndex]);
+    openGallery();
   }
 };
 
-var openGalleryPopup = function () {
+var openGallery = function () {
   document.querySelector('body').classList.add('modal-open');
-  bigPicture.classList.remove('hidden');
+  gallery.classList.remove('hidden');
 };
 
-var closeGalleryPopup = function () {
+var closeGallery = function () {
   document.querySelector('body').classList.remove('modal-open');
-  bigPicture.classList.add('hidden');
+  gallery.classList.add('hidden');
 };
 
-var buttonGalleryCloseClickHandler = function (evt) {
-  if (evt.target.className === 'big-picture__cancel cancel') {
-    closeGalleryPopup();
-  }
-};
-
-var escGalleryKeydownHandler = function (evt) {
+var galleryEscPress = function (evt) {
   if (evt.keyCode === KEYCODE_ESC) {
-    closeGalleryPopup();
+    closeGallery();
   }
 };
-document.addEventListener('keydown', escGalleryKeydownHandler);
-picturesSection.addEventListener('click', picturePreviewClickHandler);
-bigPicture.addEventListener('click', buttonGalleryCloseClickHandler);
+
+picturesSection.addEventListener('click', onPhotoClick);
+galleryCloseBtn.addEventListener('click', closeGallery);
+document.addEventListener('keydown', galleryEscPress);
 // ------ 1. Конец: Галерея
 
-// ------ 2. Начало: Загрузка фото
-var uploadSection = document.querySelector('.img-upload');
-var uploadFile = uploadSection.querySelector('#upload-file');
-var uploadOverlay = uploadSection.querySelector('.img-upload__overlay');
-var uploadCloseButton = uploadSection.querySelector('.img-upload__cancel');
-var uploadImgPreview = uploadSection.querySelector('.img-upload__preview');
 
-var openUploadPopup = function () {
+// ------ 2. Начало: Загрузка фото
+var openUpload = function () {
   document.querySelector('body').classList.add('modal-open');
   uploadOverlay.classList.remove('hidden');
+  document.addEventListener('keydown', onUploadEscPress);
 };
-
-var closeUploadPopup = function () {
+var closeUpload = function () {
   document.querySelector('body').classList.remove('modal-open');
   uploadFile.value = '';
-  setScale(scale.DEFAULT);
+  uploadImgPreview.className = 'img-upload__preview';
+  uploadImgPreview.style = '';
+  setScale('reset');
   uploadOverlay.classList.add('hidden');
 };
-
-var escUploadPopup = function (evt) {
+var onUploadEscPress = function (evt) {
   if (evt.keyCode === KEYCODE_ESC) {
-    closeUploadPopup();
+    closeUpload();
   }
 };
+uploadFile.addEventListener('change', openUpload);
+uploadCloseBtn.addEventListener('click', closeUpload);
 
-uploadFile.addEventListener('change', openUploadPopup);
-uploadCloseButton.addEventListener('click', closeUploadPopup);
-document.addEventListener('keydown', escUploadPopup);
-
-
-// ------------ 2.1 Начало: Загруженное фото - Масштаб
-var scale = {
-  RANGE: 25,
-  MIN: 25,
-  MAX: 100,
-  DEFAULT: 100
-};
-var resize = uploadSection.querySelector('.resize');
-var resizeMinus = resize.querySelector('.resize__control--minus');
-var resizePlus = resize.querySelector('.resize__control--plus');
-var resizeValue = resize.querySelector('.resize__control--value');
-
-var setScale = function (valueNum) {
+// ------------ Масштаб
+var setScale = function (command) {
+  var valueNum = resizeValue.value.slice(0, -1);
+  if ( command === 'plus' && valueNum < scale.MAX) {
+    valueNum = +valueNum + scale.RANGE;
+  }
+  if ( command === 'minus' && valueNum > scale.MIN) {
+    valueNum = +valueNum - scale.RANGE;
+  };
+  if ( command === 'reset') {
+    valueNum = scale.DEFAULT;
+  };
   uploadImgPreview.style.transform = 'scale(' + valueNum / 100 + ')';
   resizeValue.setAttribute('value', valueNum + '%');
 };
 
-var buttonResizePlusClickHandler = function () {
-  var valueNum = resizeValue.value.slice(0, -1);
-  if (valueNum < scale.MAX) {
-    setScale(valueNum = +valueNum + scale.RANGE);
-  }
-};
+resizePlus.addEventListener('click', function() {
+  setScale('plus');
+});
+resizeMinus.addEventListener('click', function() {
+  setScale('minus');
+});
 
-var buttonResizeMinusClickHandler = function () {
-  var valueNum = resizeValue.value.slice(0, -1);
-  if (valueNum > scale.MIN) {
-    setScale(valueNum = +valueNum - scale.RANGE);
-  }
-};
-
-resizePlus.addEventListener('click', buttonResizePlusClickHandler);
-resizeMinus.addEventListener('click', buttonResizeMinusClickHandler);
-// ------------ 2.1 Конец: Загруженное фото - Масштаб
-
-// ------------ 2.2 Начало: Загруженное фото - фильтры
-var DEFAULT_EFFECT_VALUE = 100;
-var uploadEffectScale = uploadSection.querySelector('.scale');
-var uploadEffectsList = uploadSection.querySelector('.effects__list');
-var uploadEffectValue = uploadSection.querySelector('.scale__value');
-var uploadEffectPin = uploadSection.querySelector('.scale__pin');
-var uploadEffectLine = uploadSection.querySelector('.scale__line');
+// ------------ Фильтры
 var effects = {
   chrome: function () {
-    return 'grayscale(' + uploadEffectValue.value / 100 + ')';
+    return 'grayscale(' + uploadEffectValue / 100 + ')';
   },
   sepia: function () {
-    return 'sepia(' + uploadEffectValue.value / 100 + ')';
+    return 'sepia(' + uploadEffectValue / 100 + ')';
   },
   marvin: function () {
-    return 'invert(' + uploadEffectValue.value + '%)';
+    return 'invert(' + uploadEffectValue + '%)';
   },
   phobos: function () {
-    return 'blur(' + uploadEffectValue.value * 3 / 100 + 'px)';
+    return 'blur(' + uploadEffectValue * 3 / 100 + 'px)';
   },
   heat: function () {
-    return 'brightness(' + (uploadEffectValue.value * 2 / 100 + 1) + ')';
+    return 'brightness(' + (uploadEffectValue * 2 / 100 + 1) + ')';
   },
   none: function () {
     return '';
   }
 };
-
 var currentEffect;
 var setCurrentEffect = function (evt) {
   if (evt.target.tagName === 'INPUT') {
@@ -281,20 +277,18 @@ var setCurrentEffect = function (evt) {
     }
 
     // Накладываем фильтр
-    uploadEffectValue.value = DEFAULT_EFFECT_VALUE; // указываем стартовый уровень
+    uploadEffectValue = DEFAULT_EFFECT_VALUE; // указываем стартовый уровень
     uploadImgPreview.style.filter = effects[currentEffect](); // выводим фильтр
   }
 };
 uploadEffectsList.addEventListener('click', setCurrentEffect);
 
-var setSaturation = function () {
-  uploadEffectValue.value = getSaturationLevel();
-  uploadImgPreview.style.filter = effects[currentEffect]();
-};
 var getSaturationLevel = function () {
   return Math.round(uploadEffectPin.offsetLeft / uploadEffectLine.offsetWidth * 100);
 };
+var setSaturation = function () {
+  uploadEffectValue = getSaturationLevel();
+  uploadImgPreview.style.filter = effects[currentEffect]();
+};
 uploadEffectPin.addEventListener('mouseup', setSaturation);
-// ------------ 2.2 Конец: Загруженное фото - фильтры
 // ------ 2. Конец: галерея
-

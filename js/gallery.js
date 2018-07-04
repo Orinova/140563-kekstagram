6f43ede6784f5ba752bd00bfd5ab030d786a7a72
Наташа <leninlives@yandex.ru>
2018-07-05 00:27:28 +0700

    объединен filters и gallery

--- js/gallery.js
+++ js/gallery.js
@@ -105,8 +105,8 @@
     for (var i = 0; i < picturesData.length; i++) {
       picturesData[i].id = i;
     }
-    // и заполняю страничку превьюшками
     appendPictures(picturesData);
+    filtersBox.classList.remove(INACTIVE_CLASS);
   };
 
   var onError = function (message) {
@@ -116,10 +116,54 @@
 
   window.backend.download(onSuccess, onError);
 
-  window.gallery = {
-    append: appendPictures
+// --- Сортировки картинок
+
+  var ACTIVE_CLASS = 'img-filters__button--active';
+  var INACTIVE_CLASS = 'img-filters--inactive';
+
+  var filtersBox = document.querySelector('.img-filters');
+  var filterBtns = filtersBox.querySelectorAll('.img-filters__button');
+  var filterActiveBtn = filtersBox.querySelector('.img-filters__button--active');
+
+  var filterMethods = {
+    'filter-popular': function () {
+      return picturesData;
+    },
+    'filter-new': function () {
+      var newPhotos = picturesData.slice(0, 10);
+      newPhotos = window.utils.getShuffled(newPhotos);
+      return newPhotos;
+    },
+    'filter-discussed': function (data) {
+      return data.sort(function (a, b) {
+        return b.comments.length - a.comments.length;
+      });
+    }
   };
+
+
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener('click', function (evt) {
+      var photos = picturesData.slice(0);
+      var currentBtn = evt.target;
+      var currentFilter = currentBtn.id;
+
+      if (!currentBtn.classList.contains(ACTIVE_CLASS)) {
+        photos = filterMethods[currentFilter](photos);
+
+        filterActiveBtn.classList.remove(ACTIVE_CLASS);
+        currentBtn.classList.add(ACTIVE_CLASS);
+        filterActiveBtn = currentBtn;
+
+        // Зачистка предыдущих фото:
+        var photosListItemElement = picturesSection.querySelectorAll('.picture__link');
+        photosListItemElement.forEach(function (item) {
+          item.parentNode.removeChild(item);
+        });
+
+        appendPictures(photos);
+      }
+    });
+  });
+
 })();
-
-
-//
